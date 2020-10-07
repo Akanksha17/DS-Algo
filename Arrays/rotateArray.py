73d9107d5439f9ed6c0af4d6a2b51b99c88fcbe0
Akanksha Priyadarshini <17mar.akanksha@gmail.com>
2020-10-07 13:43:32 +0530

    rotate array by k units

---
+++ Arrays/rotateArray.py
@@ -0,0 +1,16 @@
+# Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+class Solution:
+    @staticmethod
+    def reverse(self, nums, start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+
+    @staticmethod
+    def rotate(self, nums, k):
+        n = len(nums)
+        k %= n
+        self.reverse(nums, 0, n-1)
+        self.reverse(nums, 0, k-1)
+        self.reverse(nums, k, n-1)
